b62adf5ba60110c4e89393d119c453ce11aeceb4
captainlomik <nastiy248@mail.ru>
2023-10-17 09:56:23 +0300

    fix html

--- task_2/src/lib/CurrencyConverter.svelte
+++ task_2/src/lib/CurrencyConverter.svelte
@@ -1,5 +1,4 @@
 <script>
-
     let amount = 0.0;
     let convertFrom = "RUB";
     let finalAmount = 0;
@@ -13,55 +12,59 @@
 
     async function convertCurrency() {
         finalAmount = 0.0;
-        let url = `https://v6.exchangerate-api.com/v6/eac57033fe9c04693f0f4d97/pair/${convertFrom}/${convertTo}`
+        let url = `https://v6.exchangerate-api.com/v6/eac57033fe9c04693f0f4d97/pair/${convertFrom}/${convertTo}`;
 
-        await fetch(url).then( response => {
-            return response.json()
-        })
-        .then( data => {
-            console.log(data)
-            let rate = data.conversion_rate
-            finalAmount = rate * amount
-        })
+        await fetch(url)
+            .then((response) => {
+                return response.json();
+            })
+            .then((data) => {
+                console.log(data);
+                let rate = data.conversion_rate;
+                finalAmount = rate * amount;
+            });
     }
 </script>
 
-<div class="mt-4 rounded shadow border bg-red-50">
+<div class="wrap currency">
+    <h1 class="currency__header">Конвертор валют</h1>
 
-    <div id="currency-converter" class="px-4 py-4">
+    <div class="currency__main">
+        <div class="currancy__main-from">
+            <p>Из</p>
+            <input
+                type="number"
+                name="currencyAmount"
+                placeholder="Enter Amount"
+                bind:value={amount}
+            />
+            <select class="px-4 py-3 rounded-full" bind:value={convertFrom}>
+                {#each currencyList as cl}
+                    <option value={cl.name}>
+                        {cl.desc}
+                    </option>
+                {/each}
+            </select>
+        </div>
+        <div class="currancy__main-to">
+            <p>Перевести в</p>
 
-        <h1 class="font-bold text-3xl font-sans justify-center mb-10">
-            Currency Converter
-        </h1> <br />
-
-        <span class="mt-10">Enter Amount:</span>
-
-        <input class="px-2 py-2 focus:ring-indigo-500 focus:border-indigo-500 rounded sm:text-sm border-gray-300" type="number" name="currencyAmount" placeholder="Enter Amount" bind:value="{amount}" />
-        <br /> <br/>
-
-        <span>Convert From:</span>
-
-        <select class="px-4 py-3 rounded-full" bind:value={convertFrom} >
-            {#each currencyList as cl }
-                <option value="{cl.name}">
-                    {cl.desc}
-                </option>
-            {/each}
-        </select>
-
-        <span class="ml-10 mr-10">Convert To:</span>
-
-        <select class="px-4 py-3 rounded-full" bind:value={convertTo} on:change="{convertCurrency}">
-
-            {#each currencyList as cl }
-                <option value="{cl.name}">
-                    {cl.desc}
-                </option>
-            {/each}
-        </select><br/><br/>
-
-        <span> {amount} {convertFrom} equals {finalAmount} {convertTo}</span>
-
+            <select
+                class="px-4 py-3 rounded-full"
+                bind:value={convertTo}
+                on:change={convertCurrency}
+            >
+                {#each currencyList as cl}
+                    <option value={cl.name}>
+                        {cl.desc}
+                    </option>
+                {/each}
+            </select>
+        </div>
+        <div class="currancy__main-result">
+            <p> {amount} {convertFrom} equals {finalAmount} {convertTo}</p>
+        </div>
     </div>
-
 </div>
+
+
