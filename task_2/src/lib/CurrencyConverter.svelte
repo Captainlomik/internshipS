48e5732454c8c02e5661ccc37008b31f4026e990
captainlomik <nastiy248@mail.ru>
2023-10-17 16:53:16 +0300

    fix calc base

--- task_2/src/lib/CurrencyConverter.svelte
+++ task_2/src/lib/CurrencyConverter.svelte
@@ -1,9 +1,8 @@
-<script>
+<script lang="ts">
     // @ts-nocheck
-
     let amount = 0.0;
-    let convertFrom = "";
-    let finalAmount = 0;
+    let convertFrom = "USD";
+    let finalAmount = 0.0;
     let convertTo = "";
     let rate = 0.0;
 
@@ -14,7 +13,6 @@
     ];
 
     async function convertCurrency() {
-        finalAmount = 0.0;
         let url = `https://v6.exchangerate-api.com/v6/eac57033fe9c04693f0f4d97/pair/${convertFrom}/${convertTo}`;
 
         await fetch(url)
@@ -23,18 +21,19 @@
             })
             .then((data) => {
                 rate = data.conversion_rate;
+                finalAmount = rate * amount
             });
     }
 
-    // @ts-ignore
-    function getAmount(num) {
+    function getAmount(num: number) {
         amount = +num;
-        finalAmount = rate * amount;
+
+        if (rate !== 0) finalAmount = rate * amount;
     }
 
-    function getfinalAmount(num) {
+    function getfinalAmount(num: number) {
         finalAmount = +num;
-        amount = finalAmount / rate;
+        if (rate !== 0) amount = finalAmount / rate;
     }
 </script>
 
@@ -43,25 +42,32 @@
 
     <div class="currency__main">
         <div class="currancy__main-from">
-            <p>Из</p>
+            <p class="from__label">Из</p>
             <input
+                class="from__input"
                 type="number"
                 name="value1"
                 placeholder="Значение 1"
                 value={amount}
                 on:input={(e) => getAmount(e.target.value)}
             />
-            <select class="px-4 py-3 rounded-full" bind:value={convertFrom}>
+            <select class="from__select" 
+            bind:value={convertFrom}
+            on:change={convertCurrency}
+            >
                 {#each currencyList as cl}
                     <option value={cl.name}>
                         {cl.desc}
                     </option>
                 {/each}
             </select>
+
+            {#if rate === 0}<p>Укажите валюту для перевода</p> {/if}
         </div>
         <div class="currancy__main-to">
-            <p>Перевести в</p>
+            <p class="from__label">Перевести в</p>
             <input
+                class="from__input"
                 type="number"
                 name="value2"
                 placeholder="Значение 2"
@@ -69,7 +75,7 @@
                 on:input={(e) => getfinalAmount(e.target.value)}
             />
             <select
-                class="px-4 py-3 rounded-full"
+                class="from__select"
                 bind:value={convertTo}
                 on:change={convertCurrency}
             >
@@ -81,7 +87,9 @@
             </select>
         </div>
         <div class="currancy__main-result">
-            <p>{amount} {convertFrom} равен {finalAmount} {convertTo}</p>
+            {#if finalAmount}
+                <p>{amount} {convertFrom} равен {finalAmount} {convertTo}</p>
+            {/if}
         </div>
     </div>
 </div>
